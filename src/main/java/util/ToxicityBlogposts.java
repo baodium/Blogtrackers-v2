48d867dac472bc297c40fac073311849607cf823
leosj1 <johnsonseun15@gmail.com>
2020-12-08 21:04:17 -0800

    Jenkins deploy

--- src/main/java/util/ToxicityBlogposts.java
+++ src/main/java/util/ToxicityBlogposts.java
@@ -1,13 +1,10 @@
 package util;
 
 import java.io.BufferedReader;
-import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.sql.SQLException;
-
 import org.json.JSONObject;
 
 
